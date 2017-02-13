096d2e8f41df3c9831b9f07a4aba6e20f478e595
elijah-schow <elijah.schow@gmail.com>
2017-02-13 12:09:56 -0600

    Implement send function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,21 @@
-// YOUR CODE HERE:
+var app = {
+  init: function() {
+    app.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
+  },
+  send: function(message, success, error) {
+    $.ajax({
+      'url': app.server,
+      'type': 'POST',
+      //Message is a javascript object
+      'data': JSON.stringify(message),
+      'contentType': 'application/json',
+      //success and error are callback functions
+      'success': success,
+      'error': error,
+    });
+  },
+  fetch: function() {},
+  clearMessages: function() {},
+  renderMessage: function() {},
+  renderRoom: function() {},
+};
