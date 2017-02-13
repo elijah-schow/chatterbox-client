b3290d077c735ed111f61c1af828459e1ee88d97
elijah-schow <elijah.schow@gmail.com>
2017-02-13 15:44:42 -0600

    Render room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,9 @@
 var app = {
   init: function() {
     app.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
+    app.username = new URLSearchParams(window.location.search).get('username');
+    //Event handlers
+    $('#send .submit').on('click', app.handleSubmit);
   },
   send: function(data, success, error) {
     $.ajax({
@@ -13,17 +16,47 @@ var app = {
       'error': error,
     });
   },
-  fetch: function(data, success, error) {
+  fetch: function(success, error) {
     $.ajax({
       'url': app.server,
       'type': 'GET',
-      'data': JSON.stringify(data),
       'contentType': 'application/json',
       'success': success,
       'error': error
     });
   },
-  clearMessages: function() {},
-  renderMessage: function() {},
-  renderRoom: function() {},
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+  renderMessage: function(message) {
+    var $chat = $('<div class="chat"><span class="username"></span>: <span class="message"></span></div>');
+
+    $chat.find('.username').text(message['username']);
+    $chat.find('.message').text(message['text']);
+
+    $('#chats').append($chat);
+    $('#send .message-input').val('');
+  },
+  renderRoom: function() {
+    app.fetch(function(data) {
+      app.clearMessages();
+      data.results.forEach(app.renderMessage);
+    });
+  },
+  escape: function(string) {
+    //TODO: escape on output, to prevent XSS
+    return string;
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var message = {
+      'username': app.username,
+      'text': $('#send .message-input').val(),
+      'roomname': 'lobby'
+    };
+
+    app.send(message, app.renderRoom);
+  }
 };
+
